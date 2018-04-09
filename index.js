477ed0905c1460f5ecacb68d7e2637eded07607f
David Chambers <dc@davidchambers.me>
2018-04-09 09:53:58 +0200

    add currying section

--- index.js
+++ index.js
@@ -105,6 +105,43 @@
 //. Sanctuary can be configured to operate in garbage in, garbage out mode.
 //. Ramda cannot be configured to enforce its invariants.
 //.
+//. ### Currying
+//.
+//. Sanctuary functions are curried. There is, for example, exactly one way to
+//. apply `S.reduce` to `S.add`, `0`, and `xs`:
+//.
+//.   - `S.reduce (S.add) (0) (xs)`
+//.
+//. Ramda functions are also curried, but in a complex manner. There are four
+//. ways to apply `R.reduce` to `R.add`, `0`, and `xs`:
+//.
+//.   - `R.reduce (R.add) (0) (xs)`
+//.   - `R.reduce (R.add) (0, xs)`
+//.   - `R.reduce (R.add, 0) (xs)`
+//.   - `R.reduce (R.add, 0, xs)`
+//.
+//. Ramda supports all these forms because curried functions enable partial
+//. application, one of the library's tenets, but `f(x)(y)(z)` is considered
+//. too unfamiliar and too unattractive to appeal to JavaScript programmers.
+//.
+//. Sanctuary's developers prefer a simple, unfamiliar construct to a complex,
+//. familiar one. Familiarity can be acquired; complexity is intrinsic.
+//.
+//. The lack of breathing room in `f(x)(y)(z)` impairs readability. The simple
+//. solution to this problem, proposed in [#438][], is to include a space when
+//. applying a function: `f (x) (y) (z)`.
+//.
+//. Ramda also provides a special placeholder value, [`R.__`][], which removes
+//. the restriction that a function must be applied to its arguments in order.
+//. The following expressions are equivalent:
+//.
+//.   - `R.reduce (R.__, 0, xs) (R.add)`
+//.   - `R.reduce (R.add, R.__, xs) (0)`
+//.   - `R.reduce (R.__, 0) (R.add) (xs)`
+//.   - `R.reduce (R.__, 0) (R.add, xs)`
+//.   - `R.reduce (R.__, R.__, xs) (R.add) (0)`
+//.   - `R.reduce (R.__, R.__, xs) (R.add, 0)`
+//.
 //. ### Variadic functions
 //.
 //. Ramda provides several functions with take any number of arguments. These
@@ -5287,6 +5324,7 @@
 
 }));
 
+//. [#438]:                     https://github.com/sanctuary-js/sanctuary/issues/438
 //. [Alt]:                      v:fantasyland/fantasy-land#alt
 //. [Alternative]:              v:fantasyland/fantasy-land#alternative
 //. [Applicative]:              v:fantasyland/fantasy-land#applicative
@@ -5314,6 +5352,7 @@
 //. [Traversable]:              v:fantasyland/fantasy-land#traversable
 //. [UnaryType]:                v:sanctuary-js/sanctuary-def#UnaryType
 //. [`$.test`]:                 v:sanctuary-js/sanctuary-def#test
+//. [`R.__`]:                   http://ramdajs.com/docs/#__
 //. [`R.bind`]:                 http://ramdajs.com/docs/#bind
 //. [`R.invoker`]:              http://ramdajs.com/docs/#invoker
 //. [`Z.alt`]:                  v:sanctuary-js/sanctuary-type-classes#alt
